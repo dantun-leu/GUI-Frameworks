dde9e180985afaf679a28317d2a4ec43f5f5c81b
Denton Liu <liu.denton+github@gmail.com>
2015-03-31 22:21:25 -0400

    Add second demo and its solution.

---
+++ src/Demo2.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Demo2 {
+	JFrame frame;
+	public Demo2() {
+		frame = new JFrame("Demo2");
+		//Challenge: Make a JFrame of size 400x300 appear.
+	}
+	
+	public static void main(String[] args) {
+		new Demo2();
+	}
+}
---
+++ src/Demo2Solution.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Demo2Solution {
+	JFrame frame;
+	public Demo2Solution() {
+		frame = new JFrame("Demo2");
+		frame.setSize(400, 300);
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new Demo2Solution();
+	}
+}
