5a5a9b56ed8260a0b532f175c61a857bb0d707ba
Denton Liu <liu.denton+github@gmail.com>
2015-04-02 02:32:07 -0400

    Finished Demo7

--- src/Demo7.java
+++ src/Demo7.java
@@ -4,16 +4,38 @@ import java.awt.*;
 public class Demo7 {
 	private JFrame frame;
 	private JButton button;
+	private JCheckBox checkBox;
+	private JRadioButton radioButton;
+	private JComboBox<String> comboBox;
+	private JLabel label;
 	
 	public Demo7() {
 		frame = new JFrame("Demo7");
 		frame.setLayout(new FlowLayout());
-		button = new JButton(
-				"<html><img src=\"http://www.picturesnew.com/media/images/picture-wallpaper.jpg\" width=\"124\" height=\"93\"></html>");
-		frame.add(button);
-		frame.setSize(frame.getPreferredSize());
-		frame.setVisible(true);
 		
+		button = new JButton(
+				"<html><img src=\"https://31.media.tumblr.com/tumblr_mc2fpeCPVD1rf089no1_500.gif\" width=\"100\" height=\"88\"></html>");
+		frame.add(button);
+		
+		radioButton = new JRadioButton("<html><code>Code format</code></html>");
+		frame.add(radioButton);
+		
+		checkBox = new JCheckBox(
+				"<html><p style=\"color:orange;\">CSS Coloured text</p></html>");
+		frame.add(checkBox);
+		
+		comboBox = new JComboBox<String>(
+				new String[] {
+						"<html><img src=\"http://www.picturesnew.com/media/images/picture-wallpaper.jpg\" width=\"124\" height=\"93\"></html>",
+						"Text, too." });
+		frame.add(comboBox);
+		
+		label = new JLabel(
+				"<html><i><b>Add your own HTML code here!</b></i></html>");
+		frame.add(label);
+		
+		frame.setSize(300, 350);
+		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
